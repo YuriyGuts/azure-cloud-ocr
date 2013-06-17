2c4687f8c6d3bdd8b175fef2a79c43c619de702e
Yuriy Guts <yuriy.guts@gmail.com>
2013-06-17 19:27:55 +0300

    Added UI for image upload form. Added ReCaptcha.

--- src/AzureCloudOCR/WebRole/Controllers/HomeController.cs
+++ src/AzureCloudOCR/WebRole/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Configuration;
 using System.Web.Mvc;
+using WebRole.Models;
 
 namespace WebRole.Controllers
 {
@@ -14,6 +12,24 @@ namespace WebRole.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        //
+        // GET: /Home/UploadImage/
+
+        public ActionResult UploadImage()
+        {
+            return View(new UploadImageViewModel(WebConfigurationManager.AppSettings["recaptchaPublicKey"]));
+        }
+
+        //
+        // POST: /Home/UploadImage/
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadImage(UploadImageViewModel model)
+        {
+            return Content("TODO: Process posted data.");
         }
     }
 }
---
+++ src/AzureCloudOCR/WebRole/Models/UploadImageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace WebRole.Models
+{
+    public class UploadImageViewModel
+    {
+        private readonly string captchaPublicKey;
+
+        public UploadImageViewModel() : this(null)
+        {
+        }
+
+        public UploadImageViewModel(string captchaPublicKey)
+        {
+            this.captchaPublicKey = captchaPublicKey;
+        }
+
+        [Display(Name = "Email address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Image file")]
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        public string CaptchaPublicKey
+        {
+            get { return captchaPublicKey; }
+        }
+
+        [Display(Name = "Verification code")]
+        public string CaptchaValue { get; set; }
+    }
+}
