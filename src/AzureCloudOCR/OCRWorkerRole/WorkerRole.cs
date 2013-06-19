f9030abdae3f7df12c40b34c6c246edb99a5e5f1
Yuriy Guts <yuriy.guts@gmail.com>
2013-06-19 17:41:18 +0300

    Implemented the basic workflow of OCR Worker Role (no actual OCR yet).

--- src/AzureCloudOCR/OCRWorkerRole/WorkerRole.cs
+++ src/AzureCloudOCR/OCRWorkerRole/WorkerRole.cs
@@ -1,27 +1,59 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
+using System.Drawing;
+using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
+using AzureStorageUtils;
 using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
 
 namespace OCRWorkerRole
 {
     public class WorkerRole : RoleEntryPoint
     {
+        private bool onStopCalled;
+        private bool returnedFromRunMethod;
+
         public override void Run()
         {
-            // This is a sample worker implementation. Replace with your logic.
-            Trace.TraceInformation("OCRWorkerRole entry point called", "Information");
+            Trace.TraceInformation("OCRWorkerRole entry point called.", "Information");
 
             while (true)
             {
+                Trace.TraceInformation("OCRWorkerRole is awake.", "Information");
+                Trace.TraceInformation("OCR Queue has approximately {0} message(s).", AzureQueues.OCRQueue.ApproximateMessageCount);
+
+                if (onStopCalled)
+                {
+                    Trace.TraceInformation("OnStop request caught in Run method.");
+                    returnedFromRunMethod = true;
+                    break;
+                }
+
+                var ocrMessageVisibilityTimeout = TimeSpan.FromMinutes(3);
+                var ocrQueueRequestOptions = new QueueRequestOptions
+                {
+                    MaximumExecutionTime = TimeSpan.FromMinutes(15),
+                    RetryPolicy = new LinearRetry(TimeSpan.FromMinutes(5), 5)
+                };
+
+                while (true)
+                {
+                    var queueMessage = AzureQueues.OCRQueue.GetMessage(ocrMessageVisibilityTimeout, ocrQueueRequestOptions);
+                    if (queueMessage == null)
+                    {
+                        break;
+                    }
+
+                    ProcessOCRQueueMessage(queueMessage);
+                }
+
+                Trace.TraceInformation("No new messages to process yet. Sleeping.", "Information");
                 Thread.Sleep(10000);
-                Trace.TraceInformation("Working", "Information");
             }
         }
 
@@ -30,10 +62,116 @@ namespace OCRWorkerRole
             // Set the maximum number of concurrent connections 
             ServicePointManager.DefaultConnectionLimit = 12;
 
+            InitializeAzureStorage();
+
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
             return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("OnStop request received. Trying to stop...");
+            
+            onStopCalled = true;
+            while (!returnedFromRunMethod)
+            {
+                Thread.Sleep(1000);
+            }
+
+            Trace.TraceInformation("OnStop request received. Trying to stop...");
+        }
+
+        private void InitializeAzureStorage()
+        {
+            Trace.TraceInformation("Initializing Azure Storage.");
+
+            Trace.TraceInformation("Loading storage settings.");
+            string storageConnectionString = RoleEnvironment.GetConfigurationSettingValue("StorageConnectionString");
+            string imageBlobContainerName = RoleEnvironment.GetConfigurationSettingValue("ImageBlobContainerName");
+            string textBlobContainerName = RoleEnvironment.GetConfigurationSettingValue("TextBlobContainerName");
+            string ocrQueueName = RoleEnvironment.GetConfigurationSettingValue("OCRQueueName");
+            string emailQueueName = RoleEnvironment.GetConfigurationSettingValue("EmailQueueName");
+
+            Trace.TraceInformation("Initializing Blob Storage.");
+            AzureBlobs.Initialize(storageConnectionString, imageBlobContainerName, textBlobContainerName);
+
+            Trace.TraceInformation("Initializing Queues.");
+            AzureQueues.Initialize(storageConnectionString, ocrQueueName, emailQueueName);
+        }
+
+        private void ProcessOCRQueueMessage(CloudQueueMessage queueMessage)
+        {
+            string messageContent = queueMessage.AsString;
+            Trace.TraceInformation("Processing OCR queue message: " + messageContent);
+
+            string blobName;
+            string recipientEmail;
+
+            try
+            {
+                var splitMessage = messageContent.Split('|');
+                blobName = splitMessage[0];
+                recipientEmail = splitMessage[1];
+
+                if (string.IsNullOrEmpty(blobName) || string.IsNullOrEmpty(recipientEmail))
+                {
+                    throw new FormatException("Blob name and recipient email but be non-empty.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceInformation("Invalid OCR message format. Deleting. Details: " + ex.Message);
+                AzureQueues.OCRQueue.DeleteMessage(queueMessage);
+                return;
+            }
+
+            try
+            {
+                Image image = ReadImageFromBlob(blobName);
+                string ocrText = RecognizeTextOnImage(image);
+                CloudBlockBlob textBlob = StoreRecognizedTextAsBlob(ocrText, blobName + ".txt");
+                CreateEmailTask(textBlob.Name, recipientEmail);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceInformation("An error occurred while processing OCR message. Details: " + ex.Message);
+                return;
+            }
+
+            Trace.TraceInformation("OCR message successfully processed, deleting." + messageContent);
+            AzureQueues.OCRQueue.DeleteMessage(queueMessage);
+        }
+
+        private Image ReadImageFromBlob(string blobName)
+        {
+            var blob = AzureBlobs.ImageBlobContainer.GetBlockBlobReference(blobName);
+            var result = Image.FromStream(blob.OpenRead());
+            return result;
+        }
+
+        private string RecognizeTextOnImage(Image image)
+        {
+            // TODO: Process image with OCR engine.
+            return "This is the recognized text.";
+        }
+
+        private CloudBlockBlob StoreRecognizedTextAsBlob(string ocrText, string blobName)
+        {
+            var blob = AzureBlobs.TextBlobContainer.GetBlockBlobReference(blobName);
+            using (MemoryStream textStream = new MemoryStream(Encoding.UTF8.GetBytes(ocrText)))
+            {
+                blob.UploadFromStream(textStream);
+            }
+            return blob;
+        }
+
+        private void CreateEmailTask(string textBlobName, string recipientEmail)
+        {
+            var messageContent = string.Format("{0}|{1}", textBlobName, recipientEmail);
+            var queueMessage = new CloudQueueMessage(messageContent);
+            AzureQueues.EmailQueue.AddMessage(queueMessage);
         }
     }
 }
